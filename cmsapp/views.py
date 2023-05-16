b93d8f046e9fd58597e467cf7a824396b3cee7e2
Afi08 <afilafathima08@gmail.com>
2023-05-16 23:51:25 +0530

    first commit

---
+++ cmsapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def index(request):
+    posts = Post.objects.all()
+    context = {'posts':posts}
+    return render(request,'cmsapp/index.html',context)
+
+def detail(request,slug):
+    post = Post.objects.get(slug=slug)
+    posts = Post.objects.exclude(post_id_exact=post.post_id)[:5]
+    context={'post':post,'posts':posts}
+    return render(request,'cmsapp/detail.html',context)
+
+# Create your views here.
